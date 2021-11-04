f2776c3ceee888660347d4687fdfcf641daf33ba
Miguel Sepulcre <msepulcre@umh.es>
2021-11-04 17:21:19 +0100

    Update get_FER.m

--- get_FER.m
+++ get_FER.m
@@ -6,9 +6,11 @@ function [ FER_avg ] = get_FER ( Eb_No , PDF_Eb_No , step_dB)
     % This is an auxiliary script used by function model80211p to model the 
     % communication performance of IEEE 802.11p using the analytical models described in:
     % 
-    %    Miguel Sepulcre, Manuel Gonzalez-Martín, Javier Gozalvez, Rafael Molina-Masegosa, 
+    %    Miguel Sepulcre, Manuel Gonzalez-Martín, Javier Gozalvez, Rafael Molina-Masegosa, Baldomero Coll-Perales, 
     %    "Analytical Models of the Performance of IEEE 802.11p Vehicle to Vehicle Communications", 
-    %    Arxiv, 2021.
+    %    IEEE Transactions on Vehicular Technology, November 2021. DOI: 10.1109/TVT.2021.3124708
+    %    Final version available at: https://ieeexplore.ieee.org/document/9599363
+    %    Post-print version available at: https://arxiv.org/abs/2104.07923
     %
     % Input parameters:
     %    Eb_No: Energy per bit over noise. 
